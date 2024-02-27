b93238806cce54c5d711b0c793cffc3ca83b272b
Kristian2705 <k_petrov27@abv.bg>
2024-02-27 22:53:47 +0200

    Added SeedData class

---
+++ HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Models/SeedDatabase/SeedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models.SeedDatabase
+{
+    internal static class SeedData
+    {
+
+    }
+}
