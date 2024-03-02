1b03997285082305a992155cd66053651de6a378
Kristian2705 <k_petrov27@abv.bg>
2024-03-02 16:08:34 +0200

    adjusted namespaces

--- HouseRentingSystem/HouseRentingSystem.Core/Contracts/House/IHouseService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HouseRentingSystem.Core.Models.House;
-
-namespace HouseRentingSystem.Core.Contracts.House
-{
-	public interface IHouseService
-	{
-		Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses(); 
-	}
-}
---
+++ HouseRentingSystem/HouseRentingSystem.Core/Services/HouseService.cs
@@ -0,0 +1,31 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.House;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class HouseService : IHouseService
+    {
+        private readonly IRepository repository;
+        public HouseService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+        {
+            return await repository
+                .AllReadOnly<House>()
+                .OrderByDescending(h => h.Id)
+                .Take(3)
+                .Select(h => new HouseIndexServiceModel()
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    ImageUrl = h.ImageUrl,
+                })
+                .ToListAsync();
+        }
+    }
+}
